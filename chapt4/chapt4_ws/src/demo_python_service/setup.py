9d356b6464863156cf0850efe1bccd47eb4f82eb
ywu2014 <yejunwu123@gmail.com>
2025-08-16 09:53:52 +0000

    feat: 人脸检测服务初步搭建

---
+++ chapt4/chapt4_ws/src/demo_python_service/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+package_name = 'demo_python_service'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/resource', ['resource/default.jpg']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ywu',
+    maintainer_email='yejunwu123@gmail.com',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'learn_face_detect = demo_python_service.learn_face_detect:main',
+        ],
+    },
+)
---
+++ test/face_detect_test.py
@@ -0,0 +1,20 @@
+import face_recognition
+import cv2
+
+# 获取图片真实路径
+# 通过功能包名字获取该功能包的安装目录
+default_img_path = '/home/ywu/project/ros/learn/chapt4/chapt4_ws/src/demo_python_service/resource/default.jpg'
+print('default_img_path:', default_img_path)
+# 使用opencv加载图片
+image = cv2.imread(default_img_path)
+# 查找图像中的所有人脸
+face_locations = face_recognition.face_locations(image, number_of_times_to_upsample=1, model='hog')
+
+# 绘制每个人脸的边框
+for top, right, bottom, left in face_locations:
+    # 绘制边框
+    cv2.rectangle(image, (left, top), (right, bottom), (255, 0, 0), 4)
+# 显示图像
+cv2.imshow('Face Detect', image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
