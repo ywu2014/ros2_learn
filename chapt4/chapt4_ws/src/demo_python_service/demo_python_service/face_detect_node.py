f83d95db077dce76448b4b1fa88582f6b214bbad
ywu2014 <yejunwu123@gmail.com>
2025-08-17 11:13:07 +0000

    feat: 服务参数订阅及修改监听

--- chapt4/chapt4_ws/src/demo_python_service/demo_python_service/face_detect_node.py
+++ chapt4/chapt4_ws/src/demo_python_service/demo_python_service/face_detect_node.py
@@ -2,6 +2,7 @@ import rclpy
 from rclpy.node import Node
 from chapt4_interfaces.srv import FaceDetector
 from ament_index_python.packages import get_package_share_directory
+from rcl_interfaces.msg import SetParametersResult
 from cv_bridge import CvBridge
 import cv2
 import face_recognition
@@ -10,11 +11,29 @@ import time
 class FaceDetectorionNode(Node):
     def __init__(self):
         super().__init__('face_detect_node')
+
+        self.declare_parameter('face_locations_upsample_times', 1)
+        self.declare_parameter('face_locations_model', 'hog')
+        self.upsample_times = self.get_parameter('face_locations_upsample_times').value
+        self.model = self.get_parameter('face_locations_model').value
+
+        self.add_on_set_parameters_callback(self.parameter_callback)
+
         self.bridge = CvBridge() # open和ROS2的Image格式不兼容, 用于将opencv的Image转换为ROS2的Image
         self.service = self.create_service(FaceDetector, '/face_detect', self.face_detector_callback)
         self.default_img_path = get_package_share_directory('demo_python_service') + '/resource/default.jpg'
-        self.upsample_times = 1
-        self.model = 'hog'
+
+        # 通过代码改变自身的参数
+        # self.set_parameters([rclpy.Parameter(name='face_locations_upsample_times', type=rclpy.Parameter.Type.INTEGER, value=2)])
+
+    def parameter_callback(self, parameters):
+        for parameter in parameters:
+            self.get_logger().info('参数名: %s, 参数值: %s' % (parameter.name, parameter.value))
+            if parameter.name == 'face_locations_upsample_times':
+                self.upsample_times = parameter.value
+            if parameter.name == 'face_locations_model':
+                self.model = parameter.value
+        return SetParametersResult(successful=True)
 
     def face_detector_callback(self, request, response):
         if request.image.data:
