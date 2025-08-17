a42967318a63d5b5e9c0ebfe99d93489a6849ab7
ywu2014 <yejunwu123@gmail.com>
2025-08-17 12:32:08 +0000

    feat: 客户端设置参数

--- chapt4/chapt4_ws/src/demo_python_service/demo_python_service/face_detect_client_node.py
+++ chapt4/chapt4_ws/src/demo_python_service/demo_python_service/face_detect_client_node.py
@@ -3,6 +3,8 @@ from rclpy.node import Node
 from chapt4_interfaces.srv import FaceDetector
 from sensor_msgs.msg import Image
 from ament_index_python.packages import get_package_share_directory
+from rcl_interfaces.srv import SetParameters
+from rcl_interfaces.msg import Parameter, ParameterType, ParameterValue
 import cv2
 from cv_bridge import CvBridge
 
@@ -27,7 +29,34 @@ class FaceDetectorClient(Node):
         # 处理响应
         response = future.result()
         self.get_logger().info(f'接收到响应:图像中共有{response.number}个人脸，耗时{response.use_time}ms')
-        self.show_face_location(response)
+        # 防止显示堵塞无法多次请求
+        # self.show_face_location(response)
+
+    def call_set_parameters(self, parameters):
+        client = self.create_client(SetParameters, '/face_detect_node/set_parameters')
+        while not client.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('service not available, waiting again...')
+        request = SetParameters.Request()
+        request.parameters = parameters
+        future = client.call_async(request)
+        rclpy.spin_until_future_complete(self, future)
+        response = future.result()
+        self.get_logger().info(f'设置参数结果: {response}')
+        return response
+    
+    def update_detect_model(self, model):
+        parameter = Parameter()
+        parameter.name = 'face_locations_model'
+        new_model_value = ParameterValue()
+        new_model_value.type = ParameterType.PARAMETER_STRING
+        new_model_value.string_value = model
+        parameter.value = new_model_value
+        response = self.call_set_parameters([parameter])
+        for result in response.results:
+            if result.successful:
+                self.get_logger().info(f'参数{parameter.name}设置成功')
+            else:
+                self.get_logger().info(f'参数{parameter.name}设置失败, 失败原因: {result.reason}')
 
     def show_face_location(self, response):
         for i in range(response.number):
@@ -40,9 +69,14 @@ class FaceDetectorClient(Node):
         cv2.imshow('Face Detect', self.image)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
-def main():
-    rclpy.init()
+def main(args=None):
+    rclpy.init(args=args)
     face_detector_client = FaceDetectorClient()
+    face_detector_client.update_detect_model('hog')
     face_detector_client.send_request()
-    # rclpy.spin(face_detector_client)
+
+    face_detector_client.update_detect_model('cnn')
+    face_detector_client.send_request()
+    # face_detector_client.send_request()
+    rclpy.spin(face_detector_client)
     rclpy.shutdown()
