602e60fae5850464e111c0dcca7f860912b3404f
ywu2014 <yejunwu123@gmail.com>
2025-09-24 11:48:21 +0800

    feat: arm测试

---
+++ practice/src/arm1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+from glob import glob
+
+package_name = 'arm1'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/urdf', glob('urdf/*')),
+        ('share/' + package_name + '/launch', glob('launch/*.py')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='linkedata',
+    maintainer_email='yejunwu123@gmail.com',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
