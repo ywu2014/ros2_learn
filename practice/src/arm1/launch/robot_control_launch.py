9a1c545282fc74443f65582d4cf23eb3808ba4d6
ywu2014 <yejunwu123@gmail.com>
2025-09-25 14:41:34 +0800

    fix: No transform from [XX_Link] to [base_link]

--- practice/src/arm1/launch/robot_control_launch.py
+++ practice/src/arm1/launch/robot_control_launch.py
@@ -6,6 +6,8 @@ package_share_directory = get_package_share_directory('arm1')
 urdf_file_path = package_share_directory + '/urdf/robot_ros2_control.urdf'
 robot_desc = open(urdf_file_path, 'r').read()
 
+# print(f'robot_desc: {robot_desc}')
+
 def generate_launch_description():
     # 发布机器人描述(urdf内容)
     # 对应的topic名称为`/robot_description`
@@ -14,7 +16,15 @@ def generate_launch_description():
         executable='robot_state_publisher',
         name='robot_state_publisher',
         output='both',
-        parameters=[{'robot_description': robot_desc}]
+        parameters=[{'robot_description': robot_desc}, {'use_sim_time': True}]
+    )
+
+    # URDF中对于joint的type为非fixed的, 这个节点不能少, 少了之后会提示类似`No transform from [XX_Link] to [base_link]`
+    joint_state_publisher_node = Node(
+        package="joint_state_publisher", 
+        executable="joint_state_publisher",
+        name='joint_state_publisher',
+        output='both',
     )
     
     cm_node = Node(
@@ -47,6 +57,7 @@ def generate_launch_description():
     return LaunchDescription(
         [
             robot_desc_node,
+            joint_state_publisher_node,
             cm_node,
             cn_node,
             rviz_node,
